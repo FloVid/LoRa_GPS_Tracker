c6af2b25d139a98d77752925997cd8a1a3a403e6
Oscar Flores <FloVid@users.noreply.github.com>
2020-12-03 11:59:47 -0700

    Update RAK4631_GPS_Tracker.ino

--- RAK4631_GPS_Tracker.ino
+++ RAK4631_GPS_Tracker.ino
@@ -51,7 +51,6 @@ static lmh_callback_t lora_callbacks = {BoardGetBatteryLevel, BoardGetUniqueId, 
 										lorawan_rx_handler, lorawan_has_joined_handler, lorawan_confirm_class_handler};
 
 //OTAA keys !!!! KEYS ARE MSB !!!!
-//OTAA keys !!!! KEYS ARE MSB !!!!
 uint8_t nodeDeviceEUI[8] = {FILL WITH YOUR KEY};
 uint8_t nodeAppEUI[8] = {FILL WITH YOUR KEY}; //Oscar if Chisrpstack is used, fill with 0x00, 0x00, 0x00.....,no needed for autentication, TTN same for all nodes into the same application                                                                   
 uint8_t nodeAppKey[16] = {FILL WITH YOUR KEY};
@@ -72,7 +71,7 @@ void setup()
 {
 	pinMode(LED_BLUE, OUTPUT);
 	digitalWrite(LED_BLUE, LOW);
-  pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_GREEN, OUTPUT);
 	digitalWrite(LED_GREEN, LOW);
 
 	// Initialize LoRa chip.
@@ -88,58 +87,58 @@ void setup()
 	Serial.println("Welcome to RAK4630 LoRaWan!!!");
 	Serial.println("Type: OTAA");
 
-  #if defined(REGION_AS923)
-    Serial.println("Region: AS923");
-  #elif defined(REGION_AU915)
-    Serial.println("Region: AU915");
-  #elif defined(REGION_CN470)
-    Serial.println("Region: CN470");
-  #elif defined(REGION_CN779)
-    Serial.println("Region: CN779");
-  #elif defined(REGION_EU433)
-    Serial.println("Region: EU433");
-  #elif defined(REGION_IN865)
-    Serial.println("Region: IN865");
-  #elif defined(REGION_EU868)
-    Serial.println("Region: EU868");
-  #elif defined(REGION_KR920)
-    Serial.println("Region: KR920");
-  #elif defined(REGION_US915)
-    Serial.println("Region: US915");
-  #elif defined(REGION_US915_HYBRID)
-    Serial.println("Region: US915_HYBRID");
-  #else
-    Serial.println("Please define a region in the compiler options.");
-  #endif
-    Serial.println("=====================================");
+	#if defined(REGION_AS923)
+	Serial.println("Region: AS923");
+	#elif defined(REGION_AU915)
+	Serial.println("Region: AU915");
+	#elif defined(REGION_CN470)
+	Serial.println("Region: CN470");
+	#elif defined(REGION_CN779)
+	Serial.println("Region: CN779");
+	#elif defined(REGION_EU433)
+	Serial.println("Region: EU433");
+	#elif defined(REGION_IN865)
+	Serial.println("Region: IN865");
+	#elif defined(REGION_EU868)
+	Serial.println("Region: EU868");
+	#elif defined(REGION_KR920)
+	Serial.println("Region: KR920");
+	#elif defined(REGION_US915)
+	Serial.println("Region: US915");
+	#elif defined(REGION_US915_HYBRID)
+	Serial.println("Region: US915_HYBRID");
+	#else
+	Serial.println("Please define a region in the compiler options.");
+	#endif
+	Serial.println("=====================================");
 
-  //gps init
-  pinMode(17, OUTPUT);
-  digitalWrite(17, HIGH);
-  pinMode(34, OUTPUT);
-  digitalWrite(34, 0);
-  delay(1000);
-  digitalWrite(34, 1);
-  delay(1000);
-  Serial1.begin(9600);
-  while (!Serial1);
-  digitalWrite(LED_BLUE, HIGH);
-  delay(500);
-  digitalWrite(LED_BLUE, LOW);
-  delay(500);
-  digitalWrite(LED_BLUE, HIGH);
-  delay(500);
-  digitalWrite(LED_BLUE, LOW);
-  delay(500);
-  digitalWrite(LED_BLUE, HIGH);
-  delay(500);
-  digitalWrite(LED_BLUE, LOW);
-  delay(500);
-  Serial.println();
-  Serial.println("GPS uart init ok!");
-  Serial.println();
+	//gps init
+	pinMode(17, OUTPUT);
+	digitalWrite(17, HIGH);
+	pinMode(34, OUTPUT);
+	digitalWrite(34, 0);
+	delay(1000);
+	digitalWrite(34, 1);
+	delay(1000);
+	Serial1.begin(9600);
+	while (!Serial1);
+	digitalWrite(LED_BLUE, HIGH);
+	delay(500);
+	digitalWrite(LED_BLUE, LOW);
+	delay(500);
+	digitalWrite(LED_BLUE, HIGH);
+	delay(500);
+	digitalWrite(LED_BLUE, LOW);
+	delay(500);
+	digitalWrite(LED_BLUE, HIGH);
+	delay(500);
+	digitalWrite(LED_BLUE, LOW);
+	delay(500);
+	Serial.println();
+	Serial.println("GPS uart init ok!");
+	Serial.println();
 
-  // Set the analog reference to 3.0V (default = 3.6V)
+  	// Set the analog reference to 3.0V (default = 3.6V)
 	analogReference(AR_INTERNAL_3_0);
 
 	// Set the resolution to 12-bit (0..4096)
@@ -183,17 +182,17 @@ void loop()
 void lorawan_has_joined_handler(void)
 {
 	Serial.println("OTAA Mode, Network Joined!");
-  digitalWrite(LED_GREEN, HIGH);
-  delay(500);
-  digitalWrite(LED_GREEN, LOW);
-  delay(500);
-  digitalWrite(LED_GREEN, HIGH);
-  delay(500);
-  digitalWrite(LED_GREEN, LOW);
-  delay(500);
-  digitalWrite(LED_GREEN, HIGH);
-  delay(500);
-  digitalWrite(LED_GREEN, LOW);
+	digitalWrite(LED_GREEN, HIGH);
+	delay(500);
+	digitalWrite(LED_GREEN, LOW);
+	delay(500);
+	digitalWrite(LED_GREEN, HIGH);
+	delay(500);
+	digitalWrite(LED_GREEN, LOW);
+	delay(500);
+	digitalWrite(LED_GREEN, HIGH);
+	delay(500);
+	digitalWrite(LED_GREEN, LOW);
 
 	lmh_error_status ret = lmh_class_request(gCurrentClass);
 	if (ret == LMH_SUCCESS)
@@ -210,8 +209,7 @@ void lorawan_has_joined_handler(void)
  */
 void lorawan_rx_handler(lmh_app_data_t *app_data)
 {
-	Serial.printf("LoRa Packet received on port %d, size:%d, rssi:%d, snr:%d, data:%s\n",
-				  app_data->port, app_data->buffsize, app_data->rssi, app_data->snr, app_data->buffer);
+	Serial.printf("LoRa Packet received on port %d, size:%d, rssi:%d, snr:%d, data:%s\n", app_data->port, app_data->buffsize, app_data->rssi, app_data->snr, app_data->buffer);
 }
 
 void lorawan_confirm_class_handler(DeviceClass_t Class)
@@ -234,37 +232,37 @@ void send_lora_frame(void)
   read_GPS_data();
   if(hasGPSdata && newpos)
   {
-    uint32_t i = 0;
-    memset(m_lora_app_data.buffer, 0, LORAWAN_APP_DATA_BUFF_SIZE);
-    m_lora_app_data.port = gAppPort;
-    m_lora_app_data.buffer[i++] = loraData[0];
-    m_lora_app_data.buffer[i++] = loraData[1];
-    m_lora_app_data.buffer[i++] = loraData[2];
-    m_lora_app_data.buffer[i++] = loraData[3];
-    m_lora_app_data.buffer[i++] = loraData[4];
-    m_lora_app_data.buffer[i++] = loraData[5];
-    m_lora_app_data.buffer[i++] = loraData[6];
-    m_lora_app_data.buffer[i++] = loraData[7];
-    m_lora_app_data.buffsize = i;
+	uint32_t i = 0;
+	memset(m_lora_app_data.buffer, 0, LORAWAN_APP_DATA_BUFF_SIZE);
+	m_lora_app_data.port = gAppPort;
+	m_lora_app_data.buffer[i++] = loraData[0];
+	m_lora_app_data.buffer[i++] = loraData[1];
+	m_lora_app_data.buffer[i++] = loraData[2];
+	m_lora_app_data.buffer[i++] = loraData[3];
+	m_lora_app_data.buffer[i++] = loraData[4];
+	m_lora_app_data.buffer[i++] = loraData[5];
+	m_lora_app_data.buffer[i++] = loraData[6];
+	m_lora_app_data.buffer[i++] = loraData[7];
+	m_lora_app_data.buffsize = i;
 
-    lmh_error_status error = lmh_send(&m_lora_app_data, gCurrentConfirm);
+	lmh_error_status error = lmh_send(&m_lora_app_data, gCurrentConfirm);
 
-    if (error == LMH_SUCCESS)
-    {
-      count++;
-      Serial.printf("lmh_send ok count %d\n", count);
-    }
-    else
-    {
-      count_fail++;
-      Serial.printf("lmh_send fail count %d\n", count_fail);
-    }
+	if (error == LMH_SUCCESS)
+	{
+	count++;
+	Serial.printf("lmh_send ok count %d\n", count);
+	}
+	else
+	{
+	count_fail++;
+	Serial.printf("lmh_send fail count %d\n", count_fail);
+	}
   }
   else
   {
-    delay(1000);
-    TimerSetValue(&appTimer, LORAWAN_APP_INTERVAL);
-    TimerStart(&appTimer);
+	delay(1000);
+	TimerSetValue(&appTimer, LORAWAN_APP_INTERVAL);
+	TimerStart(&appTimer);
   }
 
 }
